89272c7ca1ced6d0c29e14ce7eeb877d275e6aea
Jaecobd1 <jaecob.d1@gmail.com>
2022-12-09 23:56:11 -0500

    updating & Deleting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoList from './components/toDoList/toDoList'
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl">CR<span className="line-through">UD</span> - TODO</h1>
+      <h1 className="text-4xl">CR<span className="">UD</span> - TODO</h1>
       <h2>Create & Read Data</h2>
       <Form />
       <TodoList />
--- src/components/toDo/toDo.js
+++ src/components/toDo/toDo.js
@@ -3,14 +3,25 @@ import React from 'react';
 
 
 function ToDo({ todo }) {
-    
+    const completeTodo = () => {
+        const todoRef = firebase.database().ref("crud-todo").child(todo.id)
 
+        todoRef.update( {
+            complete: !todo.complete
+        })
+    }
+
+    const deleteTodo = () => {
+        const todoRef = firebase.database().ref("crud-todo").child(todo.id);
+
+        todoRef.remove();
+    }
     return (
         <>
-            <div className="h-32 p-2 m-2 bg-cyan-200 w-fit mx-auto rounded-xl drop-shadow-lg text-white">
-                <h1 className="mb-4 text-red-500 font-bold">{todo.title}</h1>
-                <button className="bg-slate-400 p-2 m-2" >Delete</button>
-                <button className="bg-green-400 p-2 m-2">Complete</button>
+            <div className={todo.complete ? "h-32 p-2 m-2 bg-orange-200 w-fit mx-auto rounded-xl drop-shadow-lg text-white": "h-32 p-2 m-2 bg-cyan-200 w-fit mx-auto rounded-xl drop-shadow-lg text-white"}>
+                <h1 className={todo.complete ? 'line-through mb-4 text-red-500 font-bold': "mb-4 text-red-500 font-bold"}>{todo.title}</h1>
+                <button onClick={deleteTodo} className="bg-slate-400 p-2 m-2" >Delete</button>
+                <button className="bg-green-400 p-2 m-2" onClick={completeTodo}>{todo.complete ? 'Undo' : 'Complete' }</button>
             </div>
             </>
     );
